21e1c23ea2c1a98afe7d10d7a50f2340d7c24593
Ekaji <ekajionah@gmail.com>
2023-09-21 01:02:44 +0100

    initial commit

---
+++ config/database.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // url:'mongodb+srv://<username>:<password>:@learning.eauwn.mongodb.net/ecommerce-api?retryWrites=true&w=majority'
+  url: 'mongodb+srv://onah0x1:m3XHGmHljFaCycFC@cluster0.yqxbqau.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
+};
